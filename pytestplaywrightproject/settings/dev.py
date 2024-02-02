73aa62e0948aee17fc75b059d3279b5ff27b5d04
Coen van der Kamp <coen@fourdigits.nl>
2024-02-02 11:15:02 +0100

    wagtail start pytestplaywrightproject

---
+++ pytestplaywrightproject/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-v0h5*8iyn_emco5z&&dld)a^aemumke0++==b81@7!oxgamo1h"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
